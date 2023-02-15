6ceafa40f0a16d98a3fc9860c058d96e1326a876
Maysam Rabipour <maysam.rabipour@podro.com>
2023-02-15 15:02:30 +0330

    change sql schema

--- etc/schema.sql
+++ etc/schema.sql
@@ -1,36 +1,49 @@
-CREATE TABLE shopping_cart (
-    shopping_cart_id int not null auto_increment,
-    primary key (`shopping_cart_id`)
+CREATE TABLE product (
+    product_id INT NOT NULL AUTO_INCREMENT,
+    unit_price INT NOT NULL,
+    product_name VARCHAR(255) NOT NULL,
+    product_class ENUM('physical', 'insurance', 'service'),
+    quantity INT NOT NULL default 0,
+    PRIMARY KEY (`product_id`)
 );
 
-CREATE TABLE shopping_cart_line (
-    shopping_cart_line_id int not null auto_increment,
-    shopping_cart_id int not null,
-    primary key (`shopping_cart_line_id`),
-    foreign key shopping_cart(`shopping_cart_id`) references shopping_cart(`shopping_cart_id`)
+CREATE TABLE shopping_cart (
+    shopping_cart_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    PRIMARY KEY (`shopping_cart_id`)
 );
 
 CREATE TABLE shopping_cart_item (
-    shopping_cart_item_id int not null auto_increment,
-    shopping_cart_line_id int not null,
-    quantity int not null,
-    unit_price int not null,
-    product_name varchar(255) not null,
-    product_class varchar(255) not null,
-    primary key (`shopping_cart_item_id`),
-    foreign key shopping_cartitem(`shopping_cart_line_id`) references shopping_cart_line(`shopping_cart_line_id`)
+    shopping_cart_item_id INT NOT NULL AUTO_INCREMENT,
+    shopping_cart_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    unit_price INT NOT NULL,
+    PRIMARY KEY (`shopping_cart_item_id`),
+    FOREIGN KEY (`shopping_cart_id`) references shopping_cart(`shopping_cart_id`),
+    FOREIGN KEY (`product_id`) references product(`product_id`)
 );
 
-INSERT INTO shopping_cart (shopping_cart_id) VALUES(1);
+INSERT INTO product
+    (product_id, unit_price, product_name, product_class, quantity)
+VALUES
+    (1, 89500, 'Apple MacBook Air 13,3" (2017) MQD32N/A', 'physical', 6),
+    (2, 7990, '2-year Backup Plan Complete', 'insurance', 83),
+    (3, 64900, 'AEG FSE72710P', 'physical', 7),
+    (4, 0, 'Take old product with us', 'service', 120),
+    (5, 0, 'Place and unpack', 'service', 100),
+    (6, 9999, 'Have your product built-in', 'service', 100),
+    (7, 2249, 'Case Logic Sleeve 13.3" LAPS-113 Black', 'physical', 12);
 
-INSERT INTO shopping_cart_line (shopping_cart_line_id, shopping_cart_id) VALUES (1, 1);
-INSERT INTO shopping_cart_line (shopping_cart_line_id, shopping_cart_id) VALUES (2, 1);
-INSERT INTO shopping_cart_line (shopping_cart_line_id, shopping_cart_id) VALUES (3, 1);
+INSERT INTO shopping_cart (shopping_cart_id, user_id) VALUES (1, 1);
 
-INSERT INTO shopping_cart_item (shopping_cart_item_id, shopping_cart_line_id, quantity, unit_price, product_name, product_class) VALUES (1, 1, 1, 89500, 'Apple MacBook Air 13,3" (2017) MQD32N/A', 'physical');
-INSERT INTO shopping_cart_item (shopping_cart_item_id, shopping_cart_line_id, quantity, unit_price, product_name, product_class) VALUES (2, 1, 1, 7990, '2-year Backup Plan Complete', 'insurance');
-INSERT INTO shopping_cart_item (shopping_cart_item_id, shopping_cart_line_id, quantity, unit_price, product_name, product_class) VALUES (3, 2, 1, 64900, 'AEG FSE72710P', 'physical');
-INSERT INTO shopping_cart_item (shopping_cart_item_id, shopping_cart_line_id, quantity, unit_price, product_name, product_class) VALUES (4, 2, 1, 0, 'Take old product with us', 'service');
-INSERT INTO shopping_cart_item (shopping_cart_item_id, shopping_cart_line_id, quantity, unit_price, product_name, product_class) VALUES (5, 2, 1, 0, 'Place and unpack', 'service');
-INSERT INTO shopping_cart_item (shopping_cart_item_id, shopping_cart_line_id, quantity, unit_price, product_name, product_class) VALUES (6, 2, 1, 9999, 'Have your product built-in', 'service');
-INSERT INTO shopping_cart_item (shopping_cart_item_id, shopping_cart_line_id, quantity, unit_price, product_name, product_class) VALUES (7, 3, 2, 2249, 'Case Logic Sleeve 13.3" LAPS-113 Black', 'physical');
+INSERT INTO shopping_cart_item
+    (shopping_cart_item_id, shopping_cart_id, product_id, quantity, unit_price)
+VALUES
+    (1, 1, 1, 1, 89500),
+    (2, 1, 2, 1, 7990),
+    (3, 1, 3, 1, 64900),
+    (4, 1, 4, 1, 0),
+    (5, 1, 5, 1, 0),
+    (6, 1, 6, 1, 9999),
+    (7, 1, 7, 2, 2249);
