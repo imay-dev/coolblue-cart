4d1d9d8b90243dfaa61d8c06e70e586433f90583
Maysam Rabipour <maysam.rabipour@podro.com>
2023-02-15 15:03:39 +0330

    update tests

--- tests/Unit/ShoppingCartRepositoryTest.php
+++ tests/Unit/ShoppingCartRepositoryTest.php
@@ -4,9 +4,9 @@ declare(strict_types = 1);
 
 namespace Tests\Unit;
 
+use Coolblue\App\Entity\Product;
 use Coolblue\App\Entity\ShoppingCart;
 use Coolblue\App\Entity\ShoppingCartItem;
-use Coolblue\App\Entity\ShoppingCartLine;
 use Coolblue\App\Repository\ShoppingCartRepository;
 use PHPUnit\Framework\TestCase;
 
@@ -30,70 +30,77 @@ class ShoppingCartRepositoryTest extends TestCase
     /** @test
      * @covers ShoppingCartRepository::getShoppingCart
      * @covers ShoppingCart::getShoppingCartId
-     * @covers ShoppingCart::getLines
+     * @covers ShoppingCart::getItems
      */
     public function gets_shopping_cart_with_correct_structure(): void
     {
         $this->assertNotNull($this->shoppingCart);
         $this->assertNotNull($this->shoppingCart->getShoppingCartId());
         $this->assertIsInt($this->shoppingCart->getShoppingCartId());
-        $this->assertNotNull($this->shoppingCart->getLines());
-        $this->assertIsArray($this->shoppingCart->getLines());
+        $this->assertNotNull($this->shoppingCart->getItems());
+        $this->assertIsArray($this->shoppingCart->getItems());
     }
 
     /** @test
      * @covers ShoppingCartRepository::getItems
-     * @covers ShoppingCart::getLines
-     * @covers ShoppingCartLine::getShoppingCartLineId
+     * @covers ShoppingCart::getItems
+     * @covers ShoppingCartItem::getShoppingCartItemId
+     * @covers ShoppingCartItem::getProductId
+     * @covers ShoppingCartItem::getQuantity
+     * @covers ShoppingCartItem::getUnitPrice
+     * @covers ShoppingCartItem::getSubtotal
      */
     public function gets_items_with_correct_structure(): void
     {
-        $lines = $this->shoppingCart->getLines();
-        $this->assertNotNull($lines);
-        $this->assertIsArray($lines);
+        $items = $this->shoppingCart->getItems();
+        $this->assertNotNull($items);
+        $this->assertIsArray($items);
 
-        foreach($lines as $line) {
-            $this->assertTrue($line instanceof ShoppingCartLine);
-            $this->assertNotNull($line->getShoppingCartLineId());
-            $this->assertIsInt($line->getShoppingCartLineId());
+        foreach($items as $item) {
+            $this->assertTrue($item instanceof ShoppingCartItem);
+            $this->assertNotNull($item->getShoppingCartItemId());
+            $this->assertIsInt($item->getShoppingCartItemId());
+            $this->assertNotNull($item->getProductId());
+            $this->assertIsInt($item->getProductId());
+            $this->assertNotNull($item->getQuantity());
+            $this->assertIsInt($item->getQuantity());
+            $this->assertNotNull($item->getUnitPrice());
+            $this->assertIsInt($item->getUnitPrice());
+
+            $this->assertNotNull($item->getSubtotal());
+            $this->assertIsInt($item->getSubtotal());
         }
     }
 
 
     /** @test
      * @covers ShoppingCartRepository::getItems
-     * @covers ShoppingCart::getLines
-     * @covers ShoppingCartLine::getItems
-     * @covers ShoppingCartItem::getShoppingCartItemId
-     * @covers ShoppingCartItem::getQuantity
-     * @covers ShoppingCartItem::getUnitPrice
-     * @covers ShoppingCartItem::getProductName
-     * @covers ShoppingCartItem::getProductClass
-     * @covers ShoppingCartItem::getSubtotal
+     * @covers ShoppingCartItem::getProduct
+     * @covers Product::getProductId
+     * @covers Product::getUnitPrice
+     * @covers Product::getProductName
+     * @covers Product::getProductClass
+     * @covers Product::getQuantity
      */
     public function gets_items_products_with_correct_structure(): void
     {
-        $lines = $this->shoppingCart->getLines();
-        $this->assertNotNull($lines);
-        $this->assertIsArray($lines);
+        $items = $this->shoppingCart->getItems();
+        $this->assertNotNull($items);
+        $this->assertIsArray($items);
 
-        foreach($lines as $line) {
-            $items = $line->getItems();
-            foreach ($items as $item) {
-                $this->assertTrue($item instanceof ShoppingCartItem);
-                $this->assertNotNull($item->getShoppingCartItemId());
-                $this->assertIsInt($item->getShoppingCartItemId());
-                $this->assertNotNull($item->getQuantity());
-                $this->assertIsInt($item->getQuantity());
-                $this->assertNotNull($item->getUnitPrice());
-                $this->assertIsInt($item->getUnitPrice());
-                $this->assertNotNull($item->getProductName());
-                $this->assertIsString($item->getProductName());
-                $this->assertNotNull($item->getProductClass());
-                $this->assertIsString($item->getProductClass());
-                $this->assertNotNull($item->getSubtotal());
-                $this->assertIsInt($item->getSubtotal());
-            }
+        foreach($items as $item) {
+            $product = $item->getProduct();
+            $this->assertTrue($product instanceof Product);
+            $this->assertNotNull($product->getProductId());
+            $this->assertIsInt($product->getProductId());
+            $this->assertNotNull($product->getUnitPrice());
+            $this->assertIsInt($product->getUnitPrice());
+            $this->assertNotNull($product->getProductName());
+            $this->assertIsString($product->getProductName());
+            $this->assertNotNull($product->getProductClass());
+            $this->assertIsString($product->getProductClass());
+            $this->assertNotNull($product->getQuantity());
+            $this->assertIsInt($product->getQuantity());
         }
     }
 }
